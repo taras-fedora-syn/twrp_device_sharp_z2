45d03f5434d9971d7f2b0ada6c7628e53109ac57
taras-fedora-syn <tabulatarasa@gmail.com>
2020-11-05 13:55:21 +0200

    First commit

---
+++ omni_sharp_z2.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/sharp/sharp_z2/sharp_z2.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sharp_z2
+PRODUCT_NAME := omni_sharp_z2
+PRODUCT_BRAND := Sharp
+PRODUCT_MODEL := Sharp Z2
+PRODUCT_MANUFACTURER := Sharp
