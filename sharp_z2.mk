62f0b6a0ba304745c2719b5eb85278781a0d001b
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-09 16:59:47 +0200

    Update sharp_z2.mk

--- sharp_z2.mk
+++ sharp_z2.mk
@@ -16,16 +16,3 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := sharp
-PRODUCT_NAME := sharp_z2
-PRODUCT_BRAND := sharp
-PRODUCT_MODEL := Sharp Z2
-PRODUCT_MANUFACTURER := sharp
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-		TARGET_DEVICE="sharp" \
-		PRODUCT_NAME="sharp"
-
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
