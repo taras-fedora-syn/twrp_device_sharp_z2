8bd9ac7444730e40db4d79be085766611084e13e
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-09 23:22:59 +0200

    Update omni_sharp_z2.mk

--- omni_sharp_z2.mk
+++ omni_sharp_z2.mk
@@ -1,6 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := sharp_z2
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
