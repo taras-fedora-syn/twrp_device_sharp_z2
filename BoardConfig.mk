a20a29c953939ce8a07dedad118dbc01ea4d9963
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-09 13:32:29 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/recovery.fstab
 
 # TWRP specific build flags
 BOARD_HAS_NO_REAL_SDCARD := true
