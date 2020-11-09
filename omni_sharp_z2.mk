3b9ad803f0fa3bcb09ff8781629e5cfb25bcb365
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-09 22:22:56 +0200

    Update omni_sharp_z2.mk

--- omni_sharp_z2.mk
+++ omni_sharp_z2.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := device/sharp/sharp_z2
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
