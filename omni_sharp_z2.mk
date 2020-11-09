ab3b718960da2c7fda7333909f59e31e17f36025
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-09 20:09:33 +0200

    Update omni_sharp_z2.mk

--- omni_sharp_z2.mk
+++ omni_sharp_z2.mk
@@ -25,6 +25,6 @@ PRODUCT_MANUFACTURER := Sharp
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/init.recovery.mt6797.rc:root/init.recovery.mt6797.rc \
-    $(LOCAL_PATH)/root/init.mt6797.usb.rc:root/init.mt6797.usb.rc \
-    $(LOCAL_PATH)/root/ueventd.mt6797.rc:root/ueventd.mt6797.rc
+    $(LOCAL_PATH)/omni/device/sharp/sharp_z2/recovery/root/init.recovery.mt6797.rc:root/init.recovery.mt6797.rc \
+    $(LOCAL_PATH)/omni/device/sharp/sharp_z2/recovery/root/init.mt6797.usb.rc:root/init.mt6797.usb.rc \
+    $(LOCAL_PATH)/omni/device/sharp/sharp_z2/recovery/root/ueventd.mt6797.rc:root/ueventd.mt6797.rc
