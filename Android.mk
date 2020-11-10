ba4e97c671ccc4c1e3bdd053e8d409b059354f11
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-10 14:10:51 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
-LOCAL_PATH := $(call my-dir)
+ifneq ($(filter $(TARGET_DEVICE), sharp_z2),)
 
-ifeq ($(TARGET_DEVICE),sharp_z2)
+LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
