177cda7dc57aba88661897792f3ec6a96c9de79b
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-10 14:15:27 +0200

    Update sharp_z2.mk

--- sharp_z2.mk
+++ sharp_z2.mk
@@ -1,15 +1,22 @@
-LOCAL_PATH := device/sharp/sharp_z2
+#
+# Copyright (C) 2018 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	                                                                                 LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	                                                                                 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/kernel:kernel \
-$(LOCAL_PATH)/recovery.fstab:root/recovery.fstab
+$(call inherit-product, device/wileyfox/porridge/full_porridge.mk)
 
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_NAME := sharp_z2
+PRODUCT_NAME := omni_sharp_z2
