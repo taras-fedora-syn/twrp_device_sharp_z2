366c16df00f16398f3ac5a3be6e30399b728dfe3
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-10 09:27:28 +0200

    Update sharp_z2.mk

--- sharp_z2.mk
+++ sharp_z2.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH := device/sharp/sharp_z2
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	                                                                                 LOCAL_KERNEL := $(LOCAL_PATH)/Image.gz
+	                                                                                 LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
 	                                                                                 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/kernel:Image.gz \
+$(LOCAL_PATH)/kernel:kernel \
 $(LOCAL_PATH)/recovery.fstab:root/recovery.fstab
 
 $(call inherit-product, build/target/product/full.mk)
