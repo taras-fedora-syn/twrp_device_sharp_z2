c65871dccd606a29c462005c595331d10f4cab0c
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-10 14:23:28 +0200

    Update omni_sharp_z2.mk

--- omni_sharp_z2.mk
+++ omni_sharp_z2.mk
@@ -1,28 +1,23 @@
-# Release name
-PRODUCT_RELEASE_NAME := sharp_z2
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+#
+# Copyright (C) 2018 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += device/sharp/sharp_z2/recovery/root/etc/recovery.fstab:recovery/root/etc/recovery.fstab
+$(call inherit-product, device/sharp/sharp_z2/full_sharp_z2.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := sharp_z2
 PRODUCT_NAME := omni_sharp_z2
-PRODUCT_BRAND := Sharp
-PRODUCT_MODEL := Sharp Z2
-PRODUCT_MANUFACTURER := Sharp
 
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/init.recovery.mt6797.rc:root/init.recovery.mt6797.rc \
-    $(LOCAL_PATH)/recovery/root/init.mt6797.usb.rc:root/init.mt6797.usb.rc \
-    $(LOCAL_PATH)/recovery/root/ueventd.mt6797.rc:root/ueventd.mt6797.rc
