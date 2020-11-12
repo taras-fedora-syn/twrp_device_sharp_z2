aa5339401198306fd2121a80c7679201bcca8e0e
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-11-12 12:04:25 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_USES_LEGACY_MTK_AV_BLOB := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04f88000 --tags_offset 0x03f88000
 TARGET_PREBUILT_KERNEL := device/sharp/sharp_z2/prebuilt/kernel
