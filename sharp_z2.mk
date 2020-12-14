98924ba69cd350f5554e838c0dce60f6854d32a4
taras-fedora-syn <46643831+taras-fedora-syn@users.noreply.github.com>
2020-12-14 06:49:40 +0200

    Update sharp_z2.mk

--- sharp_z2.mk
+++ sharp_z2.mk
@@ -19,5 +19,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, device/sharp/sharp_z2/full_sharp_z2.mk)
 
-PRODUCT_NAME := omni_sharp_z2
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.rc,$(LOCAL_PATH)/recovery/root,recovery/root)
 
+PRODUCT_NAME := omni_sharp_z2
